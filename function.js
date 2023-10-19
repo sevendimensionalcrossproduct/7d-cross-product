b55e1d22dfa3ec3d5411169432e6c3e4ad64c21d
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-10-19 14:42:56 -0700

    finished animations lol

--- function.js
+++ function.js
@@ -28,3 +28,48 @@ function seven_dimensional_dot_product(x, y) {
     }
     return result;
 }
+
+function calculate_value(){
+	const x_values = Array.from(document.querySelectorAll('#x input')).map(input => parseFloat(input.value) || 0);
+	const y_values = Array.from(document.querySelectorAll('#y input')).map(input => parseFloat(input.value) || 0);
+
+	/*if(x_values.length !== 7 || y_values.length !== 7){
+		alert("bad usage");
+		return;
+	}*/
+
+	const result = seven_dimensional_cross_product(x_values, y_values);
+	console.log("hi");
+
+	return result;
+}
+
+
+function update_values(){
+	const values = calculate_value();
+	const output_elements = Array.from(document.querySelectorAll('.screen p'));
+	const animated = document.querySelector('.output');
+	const animatedElements = document.querySelectorAll('.output');
+	
+	
+
+    animatedElements.forEach((animated) => {
+        animated.classList.remove('output');
+        void animated.offsetWidth;
+        animated.classList.add('output');
+    });
+
+	
+	for (var i = 0; i < 7; i++) {
+		if (values[i].toString().length > 13){
+			output_elements[i].textContent = "Too Long!!";
+		} else {
+		output_elements[i].textContent = values[i];
+		}
+	}
+}
+
+
+
+document.querySelector('#button').addEventListener('click', update_values);
+
